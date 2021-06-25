be4a8aca7348146ba93221dd511651bbe22da319
Khachatur <casilias35@hotmail.com>
2021-06-25 03:51:27 +0200

    mea

--- components/HeadData.js
+++ components/HeadData.js
@@ -16,7 +16,7 @@ Be Happy, Be with us
       />
       <meta
         property="og:image"
-        content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
+        content="https://static.wixstatic.com/media/c90e66_7f23c531f1834e869a1468d3bcdcc291~mv2.jpeg"
       />
       {/* //Twitter */}
       <meta property="twitter:card" content="summary_large_image" />
@@ -30,7 +30,7 @@ Be Happy, Be with us
       />
       <meta
         property="twitter:image"
-        content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
+        content="https://static.wixstatic.com/media/c90e66_7f23c531f1834e869a1468d3bcdcc291~mv2.jpeg"
       />
       <link
         rel="icon"
